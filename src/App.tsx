f42b979a5c05464e9e1a7c4c0b203263fc249595
Gustav Beck-Norén <gustav.becknoren@tradera.com>
2024-12-12 16:38:13 +0100

    Replace BrowserRouter with RouterProvider & createBrowserRouter to enable view transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import { Home } from './pages/Home';
 import { Workouts } from './pages/Workouts';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import { Layout } from './components/Layout';
 import { WorkoutProvider } from '@/context/WorkoutContext';
 import { Profile } from './pages/Profile';
 import { ThemeProvider } from './components/theme-provider/theme-provider';
 import { Workout } from './components/Workout';
 
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/workouts',
+          element: <Workouts />
+        },
+        {
+          path: '/workouts/:id',
+          element: <Workout />
+        },
+        {
+          path: '/profile',
+          element: <Profile />
+        }
+      ]
+    }
+  ],
+  {
+    basename: '/hirvis-bench-press'
+  }
+);
+
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <WorkoutProvider>
-        <BrowserRouter basename="/hirvis-bench-press">
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/workouts" element={<Workouts />} />
-              <Route path="/workouts/:id" element={<Workout />} />
-              <Route path="/profile" element={<Profile />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </WorkoutProvider>
     </ThemeProvider>
   );
